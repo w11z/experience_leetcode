cd812a8cfe4d6d009a750e074f7445e53b176281
Vadym Zozulia <vadim.zozulya@gmail.com>
2025-10-21 01:00:32 +0200

    Next attempt, did pass the base case

--- 76/Problem_76/Program.cs
+++ 76/Problem_76/Program.cs
@@ -4,6 +4,8 @@ var inputs = new List<(string s, string t)>
 {
 	(s: "BRQPBFAGCRTM", t: "ABC"),
 	(s: "a", t: "aa"),
+	(s: "aaa", t: "aa"),
+	(s: "ababababac", t: "abc"),
 };
 
 foreach (var (s, t) in inputs)
@@ -14,25 +16,48 @@ foreach (var (s, t) in inputs)
 public class Solution {
 	public string MinWindow(string s, string t)
 	{
+		if (t.Length == 0)
+			return "";
+		
 		var sArray = s.ToCharArray();
 		
 		string? result = null;
 
 		for (var start = 0; start < sArray.Length; start++)
 		{
-			var tParts = new LinkedList<char>(t.ToArray());
+			var tArray = t.ToArray();
+			var tParts = new LinkedList<char>(tArray);
+			var tPartsUnique = new HashSet<char>(tArray);
+
+			int? firstEncounterIndex = null;
+			int? secondEncounterIndex = null;
 
 			for (var index = start; index < sArray.Length; index++)
 			{
 				var sChar = sArray[index];
-				if (tParts.Find(sChar) is {} encounter)
+				var foundPendingPart = tParts.Find(sChar);
+
+				if (tPartsUnique.Contains(sChar))
+				{
+					if (firstEncounterIndex is null)
+						firstEncounterIndex = index;
+					else if (secondEncounterIndex is null && foundPendingPart is null)
+						firstEncounterIndex = index;
+					else if (secondEncounterIndex is null && foundPendingPart is not null)
+						secondEncounterIndex = index;
+				}
+
+				if (foundPendingPart is {} encounter)
 				{
 					tParts.Remove(encounter);
 					if (tParts.Count == 0)
 					{
-						var length = index - start;
+						var firstEncounterIndexValue = firstEncounterIndex!.Value;
+						var length = index - firstEncounterIndexValue;
 						if (result is null || length < result.Length)
-							result = new string(sArray.Skip(start).Take(length + 1).ToArray());
+							result = new string(sArray.Skip(firstEncounterIndexValue).Take(length + 1).ToArray());
+
+						start = secondEncounterIndex ?? firstEncounterIndexValue;
 						
 						break;
 					}
